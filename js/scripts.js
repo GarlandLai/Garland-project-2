cf028a63c76c14b06f2354314478393cdb587a0e
Garland Lai <garlandlai24@gmail.com>
2019-06-07 12:12:35 -0700

    minor changes to try and make return results.. no luck

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,63 @@
 //Business Logic
 function getLanguage(answerInput, hoursInput, learnInput, animalInput, trueFalse) {
-  if (answerInput === "yes") {
-    if (hoursInput === "1") {
-      if (learnInput === "visual") {
-        if (animalInput === "dog") {
-          if (trueFalse === "True"){
-            return C.png;
-          } else {
-            return javascript.png
-          }
-        } else if (animal === "cat"){
-          if ()
-        }
-        }
-      }
-    }
+  if (answerInput === "yes" && hoursInput === "1" && learnInput === "visual" && animalInput === "dog" && trueFalse === "true"){
+    return "good job!"
+  } else if (answerInput === "yes" && hoursInput === "1" && learnInput === "visual" && animalInput === "dog" && trueFalse === "false"){
+    return go.png
+  } else if (answerInput === "yes" && hoursInput === "1" && learnInput === "visual" && animalInput === "cat" && trueFalse === "true"){
+    return javascript.png
+  } else if (answerInput === "yes" && hoursInput === "1" && learnInput === "text" && animalInput === "dog" && trueFalse === "true"){
+    return python.jpg
+  } else if (answerInput === "yes" && hoursInput === "2" && learnInput === "visual" && animalInput === "dog" && trueFalse === "true"){
+    return ruby.jpg
+  } else if (answerInput === "no" && hoursInput === "1" && learnInput === "visual" && animalInput === "dog" && trueFalse === "true"){
+    return c.png
   }
+}
+
+
+
+
+  //   if (hoursInput === "1") {
+  //     if (learnInput === "visual") {
+  //       if (animalInput === "dog") {
+  //         if (trueFalse === "True"){
+  //           return C.png;
+  //         } else {
+  //           return javascript.png
+  //         }
+  //       } else if (animal === "cat"){
+  //           return go.png
+  //         }
+  //         if (trueFalse === true) {
+  //           return ruby.jpg
+  //         } else { //trueFalse === false
+  //           return python
+  //         }
+  //       } else if (learnInput === "text"){
+  //         if (trueFalse === true) {
+  //           return go.png
+  //         }  else { //trueFalse === false
+  //           return ruby.png
+  //         }
+  //       } else if (hoursInput === "2"){
+  //           return javascript.jpg
+  //         }
+  //       if (trueFalse === true) {
+  //         return go.png
+  //         } else { //trueFalse === false
+  //         return
+  //
+  //
+  //       }if (answerInput === "no"){
+  //       if (trueFalse === true) {
+  //         return javascript.png
+  //     } else { //trueFalse === false
+  //   }
+  //       }
+  //     }
+  //   }
+  // }
 
 
 //User Interface Logic
