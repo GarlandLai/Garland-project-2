fa14d3cb83826690fa00622dd087e1da29be54aa
Garland Lai <garlandlai24@gmail.com>
2019-06-07 10:28:39 -0700

    attempt to add values to the inputs. But, I think I will may do this the other way

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
+//Business Logic
 
 
 //User Interface Logic
 $(document).ready(function(){
   $("form#survey").submit(function(event){
-    var answerInput = $("inout:radio[name=answer]:checked").val();
-    var hoursInput = $("#hours").val();
-    var learnInput = $("#learning").val();
-    var animalInput = $("input:radio[name=animal]:checked").val();
-    var trueFalse = $("input:radio[name=trueFalse]:checked").val();
-    var language = yourLanguage = (hoursInput, learnInput, animalInput, trueFalse )
+    var answerInput = parseInt($("input:radio[name=answer]:checked").val());
+    var hoursInput = parseInt($("#hours").val());
+    var learnInput = parseInt($("#learning").val());
+    var animalInput = parseInt($("input:radio[name=animal]:checked").val());
+    var trueFalse = parseInt($("input:radio[name=trueFalse]:checked").val());
+    var value = numValue(answerInput, hoursInput, learnInput, animalInput, trueFalse )
     var image = "<img src='img/' + language + ''alt='A picture of your language'"
-
-
+    $("#yourLanguage").remove();
+    $("#yourLanguage").append(image);
     event.preventDefault();
   });
 });
