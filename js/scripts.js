960acf0af942dc687660708158ca3e8e70f1b56d
Garland Lai <garlandlai24@gmail.com>
2019-06-07 08:46:13 -0700

    adds html and some basic javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 //this code will run after everything loads
 $(document).ready(function(){
-
+  $("form#survey").submit(function(event));
+  event.preventDefault();
+  var hoursInput = $("#hours").val();
 
 });
