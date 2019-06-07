8856525773d48d707b6fe09e5219f7da4ca23168
Garland Lai <garlandlai24@gmail.com>
2019-06-07 11:11:00 -0700

    attempt to add some business logic to the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
 //Business Logic
+function getLanguage(answerInput, hoursInput, learnInput, animalInput, trueFalse) {
+  if (answerInput === "yes") {
+    if (hoursInput === "1") {
+      if (learnInput === "visual") {
+        if (animalInput === "dog") {
+          if (trueFalse === "True"){
+            return C.png;
+          } else {
+            return javascript.png
+          }
+        } else if (animal === "cat"){
+          if ()
+        }
+        }
+      }
+    }
+  }
 
 
 //User Interface Logic
 $(document).ready(function(){
   $("form#survey").submit(function(event){
-    var answerInput = parseInt($("input:radio[name=answer]:checked").val());
-    var hoursInput = parseInt($("#hours").val());
-    var learnInput = parseInt($("#learning").val());
-    var animalInput = parseInt($("input:radio[name=animal]:checked").val());
-    var trueFalse = parseInt($("input:radio[name=trueFalse]:checked").val());
-    var value = numValue(answerInput, hoursInput, learnInput, animalInput, trueFalse )
+    var answerInput =$("input:radio[name=answer]:checked").val();
+    var hoursInput = $("#hours").val();
+    var learnInput = $("#learning").val();
+    var animalInput = $("input:radio[name=animal]:checked").val();
+    var trueFalse = $("input:radio[name=trueFalse]:checked").val();
+    var language = getLanguage(answerInput, hoursInput, learnInput, animalInput, trueFalse )
     var image = "<img src='img/' + language + ''alt='A picture of your language'"
     $("#yourLanguage").remove();
     $("#yourLanguage").append(image);
