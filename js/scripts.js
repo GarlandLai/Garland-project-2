3939fd0cd548aea58c54fe952e6f48ec79544f9b
Garland Lai <garlandlai24@gmail.com>
2019-06-07 09:13:57 -0700

    add survey questions using radio inputs and declar some Vars in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 //this code will run after everything loads
-$(document).ready(function(){
-  $("form#survey").submit(function(event));
-  event.preventDefault();
-  var hoursInput = $("#hours").val();
 
+
+//User Interface Logic
+$(document).ready(function(){
+  $("form#survey").submit(function(event){
+    var hoursInput = $("#hours").val();
+    var learnInput = $("#learning").val();
+    var animalInput = $("input:radio[name=animal]:checked").val();
+    var trueFalse Input = $("input:radio[name=trueFalse]:checked").val();
+
+    
+    event.preventDefault();
+  });
 });
