473abf413acfabe83f2d576f2a83bb1e53810ff2
Garland Lai <garlandlai24@gmail.com>
2019-06-07 09:46:10 -0700

    add some additional survey questions,variables, and images that I may decide to use

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-//this code will run after everything loads
 
 
 //User Interface Logic
 $(document).ready(function(){
   $("form#survey").submit(function(event){
+    var answerInput = $("inout:radio[name=answer]:checked").val();
     var hoursInput = $("#hours").val();
     var learnInput = $("#learning").val();
     var animalInput = $("input:radio[name=animal]:checked").val();
-    var trueFalse Input = $("input:radio[name=trueFalse]:checked").val();
+    var trueFalse = $("input:radio[name=trueFalse]:checked").val();
+    var language = yourLanguage = (hoursInput, learnInput, animalInput, trueFalse )
+    var image = "<img src='img/' + language + ''alt='A picture of your language'"
 
-    
+
     event.preventDefault();
   });
 });
